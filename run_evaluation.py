4a55bb8ef2ada50e0e8921062dcc6ae85c38f0c2
songheony <songheony@gmail.com>
2019-08-16 10:00:49 +0900

    add evaluation bash script

--- run_evaluation.py
+++ run_evaluation.py
@@ -33,11 +33,11 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-e", "--experts", default="None", type=str, help="experts")
+    parser.add_argument("-e", "--experts", default=[], nargs="+", help="experts")
     parser.add_argument("-d", "--dataset", default="OTB", type=str, help="dataset")
     args = parser.parse_args()
 
-    if args.experts == "None":
+    if len(args.experts) == 0:
         trackers = [
             "ATOM",
             "BACF",
--- run_evaluations.sh
+++ run_evaluations.sh
@@ -1,5 +1,2 @@
 trackers=("ATOM" "BACF" "CSRDCF" "DaSiamRPN" "ECO" "ECO_new" "MDNet" "SAMF" "SiamDW" "SiamFC" "SiamRPN" "Staple" "STRCF" "TADT" "Vital")
-len=${#trackers[@]}
-for (( i=0; i<$len; i++ )); do
-    python ./run_expert.py -e ${trackers[$i]} -d OTB
-done
+python ./run_evaluation.py -e "${trackers[@]}" -d OTB
