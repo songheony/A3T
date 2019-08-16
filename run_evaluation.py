c7b9b5f4bc8009347399339468b6bcc187e33a50
songheony <songheony@gmail.com>
2019-08-16 09:05:34 +0900

    add bash script to run all experts

--- run_evaluation.py
+++ run_evaluation.py
@@ -7,26 +7,7 @@ from datasets.lasotdataset import LaSOTDataset
 from evaluations.ope_benchmark import OPEBenchmark
 
 
-if __name__ == "__main__":
-    trackers = [
-        "ATOM",
-        # "BACF",
-        # "CSRDCF",
-        "DaSiamRPN",
-        "ECO",
-        # "MDNet",
-        # "SAMF",
-        # "SiamDW",
-        # "SiamFC",
-        # "SiamRPN",
-        # "Staple",
-        # "STRCF",
-        # "TADT",
-        # "Vital",
-    ]
-
-    dataset_name = "otb"
-
+def main(trackers, dataset_name):
     if dataset_name == "otb":
         dataset = OTBDataset()
     elif dataset_name == "nfs":
@@ -46,3 +27,35 @@ if __name__ == "__main__":
     success = benchmark.eval_success(trackers)
     precision = benchmark.eval_precision(trackers)
     benchmark.show_result(success, precision)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--experts", default="None", type=str, help="experts")
+    parser.add_argument("-d", "--dataset", default="OTB", type=str, help="dataset")
+    args = parser.parse_args()
+
+    if args.experts == "None":
+        trackers = [
+            "ATOM",
+            "BACF",
+            "CSRDCF",
+            "DaSiamRPN",
+            "ECO",
+            "ECO_new",
+            "MDNet",
+            "SAMF",
+            "SiamDW",
+            "SiamFC",
+            "SiamRPN",
+            "Staple",
+            "STRCF",
+            "TADT",
+            "Vital",
+        ]
+    else:
+        trackers = args.experts
+
+    main(trackers, args.dataset_name)
